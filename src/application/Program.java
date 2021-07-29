40e64ff47aa46b3c5d50fc2d982ac832e08762a8
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-29 20:24:22 -0300

    filtrado salario dos funcionarios cujo salario maior que um dado valor digitado pelo usuario

---
+++ src/application/Program.java
@@ -0,0 +1,63 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import model.entities.Employee;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("Enter full file path: ");
+		String strPah = sc.nextLine();
+		
+		System.out.print("Enter salary:");
+		double salaryMin = sc.nextDouble();
+		sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(strPah))){
+			
+			List<Employee> list = new ArrayList<Employee>();
+			String line = br.readLine();
+			
+			while (line != null) {
+				String[] fields = line.split(",");
+				String name = fields[0];
+				String email = fields[1];
+				Double salary = Double.parseDouble(fields[2]);
+				list.add(new Employee(name, email, salary));
+				
+				
+				line = br.readLine();
+			}			
+			
+			List<String> email = list.stream()
+					.filter(x -> x.getSalary() > salaryMin)
+					.map(x -> x.getEmail()).sorted()
+					.collect(Collectors.toList());
+			
+			email.forEach(System.out::println);
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
+		sc.close();
+		
+	}
+
+}
